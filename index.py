66b3e8aedd57b7a565961933768a1e2e56129f32
Victor Lourenço <victorslourenco@icloud.com>
2024-12-10 13:24:59 -0300

    Segundo Commit

--- index.py
+++ index.py
@@ -1,1 +1,23 @@
-print ("Ola mundo")
+def nivelDoHeroi():
+    try:
+        exp = int(input("Escreva a quantidade de experiência: "))  # Converte para inteiro
+        if exp <= 1000:
+            print("Ferro")
+        elif exp <= 2000:
+            print("Bronze")
+        elif exp <= 5000:
+            print("Prata")
+        elif exp <= 7000:
+            print("Ouro")
+        elif exp <= 8000:
+            print("Platina")
+        elif exp <= 9000:
+            print("Ascendente")
+        elif exp <= 10000:
+            print("Imortal")
+        elif exp > 10000:
+            print("Radiante")
+    except ValueError:
+        print("Por favor, digite um número válido.")  # Trata entradas inválidas
+
+nivelDoHeroi()
